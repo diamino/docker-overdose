d9dbcdc280c9f376b26dad3f04f1a18c559d8422
diamino <diamino@users.noreply.github.com>
2023-11-08 13:18:22 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="diamino"
+original_name="docker_overdose"
+original_urlname="docker-overdose"
+original_description="Awesome docker_overdose created by diamino"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv docker_overdose $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
